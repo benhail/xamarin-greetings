d0f31715d17815abd7787e715b525ff3003e9705
Benhail Acosta <benhail.acosta@gmail.com>
2016-10-06 17:57:02 -0400

    Initial check-in of module Greetings

---
+++ Greetings/GreetingsPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Xamarin.Forms;
+
+namespace Greetings
+{
+	partial class GreetingsPage : ContentPage
+	{
+		public GreetingsPage()
+		{
+			Content = new Label
+			{
+				Text = "Saludos, Xamarin.Forms()!",
+				HorizontalOptions = LayoutOptions.Center,
+				VerticalOptions = LayoutOptions.Center
+			};
+
+			/*
+			 * Adding Padding only for iOS
+			 */
+			Device.OnPlatform(iOS: () =>
+			{
+				Padding = new Thickness(0, 20, 0, 0);
+			});
+		}
+	}
+}
